6a235933ca0027248b0c2584ab8e7e44642e2919
Mokhfi Mohammed <abalahab370@gmail.com>
2026-08-26 17:24:11 +0100

    added uplaoding feature

--- Controllers/registHandler.js
+++ Controllers/registHandler.js
@@ -1,57 +1,62 @@
-const Users = require('../Data/users') ;
-const jwt = require('jsonwebtoken') ;
-const bcrypt = require("bcrypt") ;
+const Users = require('../Data/users');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
+const registHandler = async (req, res) => {
+    try {
+        const { username, password } = req.body;
 
-const registHandler = async (req , res) => {
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Username and password are required.' });
+        }
 
-            try {
-                  
-            const user = req.body ;
-      
-            if (!user.username || !user.password) {
-                  return res.sendStatus(401);
-            }
-            
-            const founduser = await Users.findOne( {
-                  username : user.username 
-            }).exec() ;
-            const duplicate = founduser ;
+        const duplicate = await Users.findOne({ username }).exec();
+        if (duplicate) {
+            return res.status(409).json({ message: 'This username already exists. Choose a different one.' });
+        }
 
-            if (duplicate) {
-                  return res.status(409).json({'message' :'This username already exist choose a different one'});
-            }
+        // 1. Hash password
+        const hashpwd = await bcrypt.hash(password, 10);
 
-            const hashpwd = await bcrypt.hash(user.password , 10) ;
-            console.log(user.password) ;
-            const accessToken = jwt.sign(
-                  {"userInfo" : {
-                         username : user.username 
-                  }} ,
-                          process.env.ACCESS_TOKEN_SECRET ,
-                          { expiresIn : '2h'}
-                  );
-            
-            const refreshToken = jwt.sign(
-                        { "username" : user.username } ,
-                        process.env.REFRESH_TOKEN_SECRET ,
-                        { expiresIn : '6h'}
-                  );
-      
-            const newuser = Users.create ( {
-                  username : user.username ,
-                  password : hashpwd ,
-                  refreshToken : refreshToken
-            })
+        // 2. Sign Refresh Token first
+        const refreshToken = jwt.sign(
+            { username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
 
-            res.cookie( 'jwt' , refreshToken , { httpOnly : true , maxAge : 24 * 60 * 60 * 1000})
-            res.json ({accessToken}) ;
-            } catch (err){
+        // 3. MUST AWAIT user creation to get the generated _id
+        const newuser = await Users.create({
+            username,
+            password: hashpwd,
+            refreshToken
+        });
 
-                  console.error(`Sir We have an error in Registring users !`);
+        // 4. Sign Access Token with the generated _id
+        const accessToken = jwt.sign(
+            {
+                userInfo: {
+                    username: newuser.username,
+                    userId: newuser._id
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '2h' }
+        );
 
-            } 
-}
+        // 5. Send Cookie and JSON Access Token
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            sameSite: 'lax',
+            maxAge: 24 * 60 * 60 * 1000
+        });
 
+        res.status(201).json({ accessToken });
 
-module.exports = registHandler ;
+    } catch (err) {
+        console.error('Sir We have an error in Registering users!', err);
+        res.status(500).json({ message: 'Registration server error.' });
+    }
+};
+
+module.exports = registHandler;
