6091304bc1e1759dca8dd5566928269c55df4eaf
Mokhfi Mohammed <abalahab370@gmail.com>
2026-09-01 13:20:44 +0100

    added deleting pic feature

--- routing/images.js
+++ routing/images.js
@@ -5,11 +5,14 @@ const router = express.Router() ;
 const path = require("path") ;
 const getGalleryController = require("../Controllers/getGalleryController") ;
 const transformController = require("../Controllers/transformController") ;
+const deleteImageController = require("../Controllers/deletingController") ;
 
 router.get ('/' , getGalleryController ) ;
 
 router.post ('/' , multer.single('image') , uplaodController ) ;
 
 router.post ( '/:id/transform' , transformController ) ;
+
+router.delete('/:id', deleteImageController) ;
 
 module.exports = router ;
